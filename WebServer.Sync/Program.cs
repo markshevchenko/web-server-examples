b4f1d93068f8f0ccb57496bdf0cf936b8daaf94d
Mark Shevchenko <markshevchenko@gmail.com>
2019-11-18 05:46:20 +0300

    Создал проект синхронного сервера

---
+++ WebServer.Sync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebServer.Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
